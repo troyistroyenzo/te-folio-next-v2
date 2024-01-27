1e3e96a87f50bb448083ac2800c967cb935fa2f2
Troy Enzo <yortozne@gmail.com>
2024-01-27 10:39:00 +0800

    added navbar, integrated prismic

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,40 @@
 "use client"
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar"
+import { ChakraProvider } from "@chakra-ui/provider";
 
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
-    </html>
+    <head>
+      <meta charSet="utf-8" />
+      <title>Troy Enzo</title>
+    </head>
+    <body className={inter.className}>
+     <ChakraProvider>
+          <Navbar/>
+          {children}
+          {/* <Footer/> */}
+     </ChakraProvider>
+          
+      </body>
+  </html>
   );
+}
+
+
+// Call request - Prismic
+export async function getServerSideProps () {
+  const articles = await Client().query(
+    Prismic.Predicates.at("document.type", "featured")
+  );
+  console.log(articles);
+
+  return {
+    props : { }
+  };
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,25 @@
 "use client"
+import Prismic from "prismic-javascript"
 import Image from "next/image";
 import Hero from "../components/Hero"
+import { Client } from "@/prismic-configuration";
+
 
 export default function Home() {
   return (
+    <div>Page</div>
     <Hero/>
   );
 }
+
+// Call request - Prismic
+// export async function getServerSideProps () {
+//   const articles = await Client().query(
+//     Prismic.Predicates.at("document.type", "featured")
+//   );
+//   console.log(articles);
+
+//   return {
+//     props : { }
+//   };
+// }
